4e73b4bce788cd99f250b63d6d263ab5488dd0df
Cameron Romo <cromo1225@gmail.com>
2022-11-28 23:16:03 -0700

    Fun w/ datatypes

---
+++ data_types.rb
@@ -0,0 +1,40 @@
+# puts "Hello World"
+
+# my_name = "Cameron Romo"
+
+# p my_name
+
+# Numbers and Math
+# sum_numbers =  10 + 10
+# p sum_numbers
+
+# square_sum = sum_numbers ** 2
+
+# p square_sum
+
+# Strings
+# first_name = "Cameron"
+# last_name = "Romo"
+# full_name = first_name + " " + last_name
+
+# full_name_interp = "#{first_name} #{last_name}"
+# p full_name_interp
+
+# puts "What's your name?"
+
+# user_name = gets.chomp
+
+# puts "Hello, #{user_name}."
+# puts "Pleasure to make your acquaintance."
+
+# Variables and Expressions
+x = 7
+y = 11
+
+sum = x + y
+
+puts sum
+
+x = 5
+
+puts sum
