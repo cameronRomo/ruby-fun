5c7111432f15c73c1c64fef84e14d94da09fac78
Cameron Romo <cromo1225@gmail.com>
2022-12-02 22:15:12 -0700

    Add while and until loops

---
+++ loops.rb
@@ -0,0 +1,29 @@
+# While loop
+counter = 0
+
+while counter <= 10 do
+  puts counter
+  counter += 1
+end
+
+# Off by one errors
+
+numbers = [0, 1, 2, 3, 4, 5]
+
+index = 0
+
+while index <= numbers.length do
+  puts numbers[index]
+  index += 1
+end
+
+# Until: checks that the condition is false
+
+bananas = 0 
+
+until bananas >= 100 do
+  bunch = rand 6
+  bananas += bunch
+  puts "I have #{bananas} bananas"
+end
+
