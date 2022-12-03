f220d35db87ad324ee18a20369043832d8e635e7
Cameron Romo <cromo1225@gmail.com>
2022-12-03 13:17:33 -0700

    Class methods

--- objects_and_classes.rb
+++ objects_and_classes.rb
@@ -49,45 +49,100 @@ test_string = "cowboy"
 # Getters let you read an instance variable
 # Setters let you change an instance variable
 
-class Dog
-  def speak
-    puts "BARK!"
+# class Dog
+#   def speak
+#     puts "BARK!"
+#   end
+# end
+
+# class Shape; end
+# class Rectangle < Shape; end
+# class Square < Rectangle; end
+# class Triangle < Shape; end
+# class IsocelesTriangle < Triangle; end
+
+# square = Square.new
+# p square.is_a? Triangle
+# p square.is_a? Rectangle
+
+# # Subclass is a specific kind of the superclass
+# # Classic Argument
+# # All Men are Mortal
+# # Socrates is a Man:
+
+# # Therefore:
+# # Socrates is mortal
+
+# class Mortal
+#   def initialize
+#     @alive = true
+#   end
+
+#   def die
+#     @alive = false
+#   end
+# end
+# class Man < Mortal; end
+
+# socrates = Man.new
+# p socrates.is_a? Mortal
+# p socrates.class.ancestors
+
+# p socrates
+# p socrates.die
+# p socrates
+
+class Person
+  def initialize name
+    @name = name
+  end
+
+  def name
+    @name
+  end
+
+  def introduce 
+    puts "Hi, I'm #{@name}."
   end
 end
 
-class Shape; end
-class Rectangle < Shape; end
-class Square < Rectangle; end
-class Triangle < Shape; end
-class IsocelesTriangle < Triangle; end
+dave = Person.new "Dave"
 
-square = Square.new
-p square.is_a? Triangle
-p square.is_a? Rectangle
+tom = Person.new "Tom"
+p dave
+p dave.name
 
-# Subclass is a specific kind of the superclass
-# Classic Argument
-# All Men are Mortal
-# Socrates is a Man:
+dave.introduce
+tom.introduce
 
-# Therefore:
-# Socrates is mortal
+p Person.class
 
-class Mortal
-  def initialize
-    @alive = true
+class Person
+  def self.introduce
+    puts "I'm the abstract concept of a person"
   end
 
-  def die
-    @alive = false
+  class << self
+    def default_man
+      self.new "John Doe"
+    end
+
+    def default_woman
+      self.new "Jane Doe"
+    end
   end
 end
-class Man < Mortal; end
 
-socrates = Man.new
-p socrates.is_a? Mortal
-p socrates.class.ancestors
+person = Person.new "Anonymous"
+person.introduce # calls instance method
+Person.introduce # Calls class method
 
-p socrates
-p socrates.die
-p socrates
+class Lawyer < Person
+end
+
+val = Lawyer.new "Val"
+p val.introduce
+Lawyer.introduce
+
+p Person.default_man
+p Lawyer.default_woman
